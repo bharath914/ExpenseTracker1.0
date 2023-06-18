12c6bf57400966522eaa8ab0a2d4c37ded187bcf
Bharath <prakash.ayinala7@gmail.com>
2023-06-18 10:33:03 +0530

    Settings Page Updated and just need to add change month feature that's it for the app

---
+++ app/src/main/java/com/bharath/expensetracker/common/Settings.kt
@@ -0,0 +1,13 @@
+package com.bharath.expensetracker.common
+
+import androidx.compose.ui.graphics.Color
+import com.bharath.expensetracker.ui.theme.Purple80
+
+object Settings {
+
+
+    val backgroundCOlor = Color(0xFF040404)
+    val calendarStyle = 0
+    var uiColor = Purple80
+
+}
